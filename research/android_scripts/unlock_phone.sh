7feae5c666ba91365f85a287108426a9478abfbf
Shaun <lecoffeestain@gmail.com>
2020-11-01 12:06:21 +0000

    Delete unlock_phone.sh

--- research/android_scripts/unlock_phone.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-adb shell input keyevent 26 #Pressing the lock button
-adb shell input touchscreen swipe 930 880 930 380 #Swipe UP
-adb shell input text 74286 # Enter pin
-adb shell input keyevent 66 # OK
-
